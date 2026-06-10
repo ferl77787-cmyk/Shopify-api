63b0e58ee64d996614d747ca03b237368722c4b0
ferl77787-cmyk <ferl77787@gmail.com>
2026-06-10 14:07:29 +0530

    Add files via upload

--- Autoshopify.py
+++ Autoshopify.py
@@ -1079,4 +1079,5 @@ def shopify_checker():
         }), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
